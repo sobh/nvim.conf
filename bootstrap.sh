8fd0451d37ea852e8a542629861a4005d0f43905
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2020-04-21 21:00:59 +0300

    Overhaul, and clean-up.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,41 +1,30 @@
-#!/usr/bin/env sh
-#
-# File:		bootstrap.sh
-#
-# Purpose:	Bootstrap configuration files by:
-#		1. Populating the home directory with symlinks to the actual
-#		dotfils.
-#
-# Author:	Mohamed Sobh <mohamed.alhusieny@gmail.com>
-# 
-# Created:	2014-08-30
-# Modified:	
+#!/bin/dash
+# Description:	Bootstrap the dotfiles
 #
 
 
 #---- Variables ----------------------------------------------------------------
-dir=$PWD			# dotfiles directory
-olddir=~/dotfiles_old		# old dotfiles backup directory
-# Dotfiles to manage
-files="bashrc vimrc vim xinitrc"
-# zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+bname=$(basename $0)
+repo_dir=$(dirname $0)			# dotfiles repository directory
+backup_dir=$HOME/dotfiles_backup/$(date "+%Y-%m-%d_%H%M%S")	# old dotfiles backup directory
 
+target_dir=$HOME
+ignored_files="($bname|\.git|README|TODO|LICENSE)"
 
-#---- Main ---------------------------------------------------------------------
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in $HOME ..."
-mkdir -pv $olddir
+#---- The money shot -----------------------------------------------------------
+cd $repo_dir
+# Synthesis a list of file to link to
+files=$(ls --almost-all | egrep -v $ignored_files)
+files_path=$(realpath --no-symlinks $files)
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-
-# Move any existing dotfiles in homedir to dotfiles_old directory, then create
-# symlinks from the homedir to any files in the ~/dotfiles directory specified
-# in $files
+# Backup
+echo "Creating '$backup_dir' for backup of any existing dotfiles in $target_dir ..."
+mkdir -pv $backup_dir
+cd $target_dir
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv -v ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+	mv -vf "$file" $backup_dir 2>&1 1>/dev/null
 done
+# Create the symlinks
+echo "Creating symlinks in '$target_dir'..."
+ln -s $files_path $target_dir
+
