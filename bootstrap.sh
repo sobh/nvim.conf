296b1a1ed4240f129d2d1d50e87aa25758f33e84
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2021-06-19 12:18:06 +0300

    Removed the 'ignored files pattern', and only link files that starts with a dot.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,29 +8,28 @@ bname=$(basename $0)
 rdir=$(realpath $(dirname $0) )			# dotfiles repository directory
 bdir=$HOME/dotfiles_backup/$(date "+%Y-%m-%d_%H%M%S")	# old dotfiles backup directory
 
-ignored_files="($bname|\.git|README|TODO|LICENSE)"
-
 #---- Main ---------------------------------------------------------------------
 IFS=$'\n'
-# Create directory structure for the configuration repository
-echo "Creating the 'dotfiles' directory structure..."
-find $rdir -type d ! -path "*/.git*" |  \
-	sed "s|$rdir|$HOME|g" |	    \
-	xargs mkdir -p
-
 # Get Symlinks
-links=$(find $rdir -type l ! -path "*/.git*" -printf "%P\n")
+links=$(find $rdir -type l -path "$rdir/.*" ! -path "*/.git*" -printf "%P\n")
 # Synthesis a list of file to link to
-files=$(find $rdir -type f ! -path "*/.git*" -printf "%P\n" | egrep -v "$ignored_files" )
-all_files=$links$'\n'$files
+files=$(find $rdir -type f -path "$rdir/.*" ! -path "*/.git*" -printf "%P\n")
+
 
 # Backup Direcotry
 echo "Backing-up the existing dotfiles to the directory: '$rdir'"
+all_files=$links$'\n'$files
 for file in $all_files; do
 	tdir="$bdir/$(dirname $file)"
 	mkdir -p $tdir
 	mv -f "$HOME/$file" "$bdir/$file" 2>/dev/null
 done
+
+# Create directory structure for the configuration repository
+echo "Creating the 'dotfiles' directory structure..."
+find $rdir -type d ! -path "*/.git*" |  \
+	sed "s|$rdir|$HOME|g" |	    \
+	xargs mkdir -p
 
 # Copy symlinks
 echo "Copying symlinks from '$rdir' to '$HOME'"
