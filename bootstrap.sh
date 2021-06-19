63ce4216eba1542638b21aafa3acf01d7daf78f7
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2021-06-19 11:57:35 +0300

    Reverted to the 'individual file linking' method, from the 'top repo directory' link method.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,46 @@
-#!/bin/dash
+#!/bin/sh
 # Description:	Bootstrap the dotfiles
 #
 
 
 #---- Variables ----------------------------------------------------------------
 bname=$(basename $0)
-repo_dir=$(dirname $0)			# dotfiles repository directory
-backup_dir=$HOME/dotfiles_backup/$(date "+%Y-%m-%d_%H%M%S")	# old dotfiles backup directory
+rdir=$(realpath $(dirname $0) )			# dotfiles repository directory
+bdir=$HOME/dotfiles_backup/$(date "+%Y-%m-%d_%H%M%S")	# old dotfiles backup directory
 
-target_dir=$HOME
 ignored_files="($bname|\.git|README|TODO|LICENSE)"
 
-#---- The money shot -----------------------------------------------------------
-cd $repo_dir
-# Synthesis a list of file to link to
-files=$(ls --almost-all | egrep -v $ignored_files)
-files_path=$(realpath --no-symlinks $files)
+#---- Main ---------------------------------------------------------------------
+IFS=$'\n'
+# Create directory structure for the configuration repository
+echo "Creating the 'dotfiles' directory structure..."
+find $rdir -type d ! -path "*/.git*" |  \
+	sed "s|$rdir|$HOME|g" |	    \
+	xargs mkdir -p
 
-# Backup
-echo "Creating '$backup_dir' for backup of any existing dotfiles in $target_dir ..."
-mkdir -pv $backup_dir
-cd $target_dir
-for file in $files; do
-	mv -vf "$file" $backup_dir 2>&1 1>/dev/null
+# Get Symlinks
+links=$(find $rdir -type l ! -path "*/.git*" -printf "%P\n")
+# Synthesis a list of file to link to
+files=$(find $rdir -type f ! -path "*/.git*" -printf "%P\n" | egrep -v "$ignored_files" )
+all_files=$links$'\n'$files
+
+# Backup Direcotry
+echo "Backing-up the existing dotfiles to the directory: '$rdir'"
+for file in $all_files; do
+	tdir="$bdir/$(dirname $file)"
+	mkdir -p $tdir
+	mv -f "$HOME/$file" "$bdir/$file" 2>/dev/null
 done
-# Create the symlinks
-echo "Creating symlinks in '$target_dir'..."
-ln -s $files_path $target_dir
+
+# Copy symlinks
+echo "Copying symlinks from '$rdir' to '$HOME'"
+for link in $links; do
+	cp -P "$rdir/$link" "$HOME/$link"
+done
+
+# Create the symlinks of files
+echo "Creating symlinks in '$HOME'..."
+for file in $files; do
+	ln -s "$rdir/$file" "$HOME/$file"
+done
 
