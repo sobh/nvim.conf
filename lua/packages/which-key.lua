6b835af3b8d22eadda9d96e8d1b2afa0d24e5aa1
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2023-05-25 02:19:34 +0300

    Added new package 'which-key'.

---
+++ lua/packages/which-key.lua
@@ -0,0 +1,21 @@
+-- Package:	Which-Key
+-- Description:	Display oa popup with possible key bindings of the typed
+--		command.
+-- URL:		https://github.com/folke/which-key.nvim
+--
+return {
+	'folke/which-key.nvim', opts = {},
+	event = "VeryLazy",
+	init = function()
+		-- Timeout
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	config = function()
+		local wk = require('which-key')
+		wk.register({
+			f = 'Fuzzy Finder',
+		}, { prefix = '<leader>' })
+	end,
+}
+
