7e77f557f9f933a14f5f5a177d48c46188df55c5
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2023-05-25 01:28:54 +0300

    Added configuration.

--- lua/packages/treesitter.lua
+++ lua/packages/treesitter.lua
@@ -10,5 +10,17 @@ return {
 		require('nvim-treesitter.install').update({ with_sync = true })
 	end,
 	config = function()
+		require('nvim-treesitter.configs').setup {
+			-- Installed Languages
+			ensure_installed = { 'c', 'cpp', 'go', 'javascript', 'lua', 'python', 'rust', 'vim', 'vimdoc', },
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
+
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = true,
+
+			highlight = { enable = true },
+		}
 	end
 }
