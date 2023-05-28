12e94b9064e6c149515ff5390d031fa2f6f008c6
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2023-05-28 23:32:40 +0300

    Disabled 'treesitter' for 'markdown'.

--- lua/packages/available/treesitter.lua
+++ lua/packages/available/treesitter.lua
@@ -70,24 +70,32 @@ local kickstart_config = function()
 	}
 end
 
+local sobh_config = function()
+	require('nvim-treesitter.configs').setup {
+		-- Installed Languages
+		ensure_installed = { 'c', 'cpp', 'go', 'javascript', 'lua', 'python', 'rust', 'vim', 'vimdoc', },
+		-- Install parsers synchronously (only applied to `ensure_installed`)
+		sync_install = false,
+
+		-- Automatically install missing parsers when entering buffer
+		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+		auto_install = true,
+
+		-- List of parsers to ignore installing (for "all")
+		ignore_install = { 'markdown' },
+
+		highlight = {
+			enable = true,
+			disable = { 'markdown' },
+		},
+	}
+end
+
 return {
 	'nvim-treesitter/nvim-treesitter',
 	dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
 	build = function()
 		require('nvim-treesitter.install').update({ with_sync = true })
 	end,
-	config = function()
-		require('nvim-treesitter.configs').setup {
-			-- Installed Languages
-			ensure_installed = { 'c', 'cpp', 'go', 'javascript', 'lua', 'python', 'rust', 'vim', 'vimdoc', },
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			highlight = { enable = true },
-		}
-	end
+	config = sobh_config,
 }
