489656bf56520f4a4e6dda2db8e6f08e5b1f8913
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-02-18 05:52:28 +0200

    Clean-up, and made the plugin load on the "VeryLazy" event.

--- lua/packages/available/ui/dressing.lua
+++ lua/packages/available/ui/dressing.lua
@@ -4,18 +4,6 @@
 return {
 	'stevearc/dressing.nvim',
 	opts = {},
-	-- lazy = true,
-	-- init = function()
-	-- 	---@diagnostic disable-next-line: duplicate-set-field
-	-- 	vim.ui.select = function(...)
-	-- 		require("lazy").load({ plugins = { "dressing.nvim" } })
-	-- 		return vim.ui.select(...)
-	-- 	end
-	-- 	---@diagnostic disable-next-line: duplicate-set-field
-	-- 	vim.ui.input = function(...)
-	-- 		require("lazy").load({ plugins = { "dressing.nvim" } })
-	-- 		return vim.ui.input(...)
-	-- 	end
-	-- end,
+	event = "VeryLazy",
 }
 
