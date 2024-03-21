61f9494e8d1c9c619490b2db771afacc4885bb65
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-03-22 00:04:01 +0200

    NeoVim | Seperated options into a seperate file to be 'reuired' from the main 'init.lua'.

--- init.lua
+++ init.lua
@@ -75,6 +75,14 @@ require('lazy').setup({
 	--     -> https://github.com/folke/lazy.nvim#-structuring-your-plugins
 }, {})
 
+---- UI ------------------------------------------------------------------------
+-- local theme = 'rose-pine'
+vim.opt.background='light'
+local theme = 'caironoon'
+if not pcall(vim.cmd.colorscheme, theme) then
+	vim.cmd('colorscheme slate')	-- Fallback to a NeoVim bundled coloscheme
+end
+
 ---- Load non-migrated Vim Configuration -------------------------------------------------------------------------------
 local vimrc = vim.fn.stdpath('config')..'/vimrc.vim'
 vim.cmd.source(vimrc)
@@ -83,49 +91,8 @@ vim.cmd.source(vimrc)
 local mappings = require('mappings')
 mappings.load(mappings.general)
 
----- Options -----------------------------------------------------------------------------------------------------------
--- See `:help vim.o`
+---- Autocommands ------------------------------------------------------------------------------------------------------
 
----- UI ------------------------------------------------------------------------
--- local theme = 'rose-pine'
-vim.o.background='dark'
-local theme = 'rose-pine'
-if not pcall(vim.cmd.colorscheme, theme) then
-	vim.cmd('colorscheme slate')	-- Fallback to a NeoVim bundled coloscheme
-end
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
-
-vim.opt.laststatus = 3                  -- Global Status Line for all windows
-vim.o.wrap = false			-- Disable wrapping long lines
--- Enable break indent for wrapped lines (Just in case)
-vim.o.breakindent = true
-
--- Set Cursor Shape
-vim.opt.guicursor = {
-	'n-v-c-sm:block',
-	'i-ci-ve:ver25-blinkon500-blinkoff500',
-	'r-cr-o:hor20-blinkon500-blinkoff500',
-}
-vim.o.mouse = 'a'			-- Enable mouse mode
-
-vim.o.textwidth = 80
-vim.o.colorcolumn = '+1,+21,+41'	-- Highlight the end of 'textwidth'
-
--- Gutter
-vim.o.number = true			-- Enable Line Numbers
-vim.o.relativenumber = true		-- Show line numbers relative
-vim.wo.signcolumn = 'yes'		-- Keep signcolumn on by default
-
--- Folding
-vim.o.foldcolumn = '4'
-vim.opt.fillchars:append { foldopen = '▼', foldclose = '►' }
-vim.o.foldmethod='expr'
-vim.o.foldexpr='nvim_treesitter#foldexpr()'
-vim.o.foldlevel = 100
-
--- vim.o.foldenable = false		-- Disable folding at startup.
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
@@ -137,30 +104,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	pattern = '*',
 })
 
--- Tabs and Indentations -------------------------------------------------------
-vim.o.tabstop     = 8      -- Number of spaces of the <Tab>
-vim.o.shiftwidth  = 8      -- Number of spaces for indents ('cinednt', '<<', '>>')
-vim.o.expandtab   = false  -- Do not expands <Tab> into spaces
-vim.o.softtabstop = 8      -- Number of space of <Tab> while editing
-
-
----- Searching  ----------------------------------------------------------------
-vim.o.hlsearch   = true   -- Highlihgt Search
-vim.o.incsearch  = true   -- Incremental Search
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase  = true
-
----- Other ---------------------------------------------------------------------
--- Set completeopt to have a better completion experience
-vim.opt.completeopt = { 'menu', 'menuone', 'preview', 'noselect' }
-
-vim.o.undofile = true   -- Save undo history
-
--- Timeout options are set by which-key
-vim.o.timeout = true   -- Enable timeout
-vim.o.timeoutlen = 300   -- Key Sequence timeout in mSec
-
--- Sync clipboard between OS and Neovim.
--- vim.o.clipboard = 'unnamedplus'
-
+---- Options -----------------------------------------------------------------------------------------------------------
+require('config.options')
