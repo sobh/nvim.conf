84250c89c7046e04e3fea24604f571e8f278d72b
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-03-30 01:00:30 +0200

    NeoVim | Config the 'Indent-Blankline' plugin to have backup highlights, if not supported by the color scheme.

--- lua/packages/ui/indent-blankline.lua
+++ lua/packages/ui/indent-blankline.lua
@@ -10,48 +10,37 @@ return {
 		-- highlights.
 		lazy = true,
 		event = 'Syntax',
-		-- If the highlights are not supported by the used colorscheme,
-		-- then you'll have to use the config function below.
-		opts = {
-			indent = {
-				highlight = {
+		config = function()
+			local ibl = require("ibl")
+			local highlight = {
 				"RainbowRed",
-				"RainbowCyan",
 				"RainbowYellow",
 				"RainbowBlue",
-				"RainbowGreen",
 				"RainbowOrange",
+				"RainbowGreen",
 				"RainbowViolet",
-			},
-			},
-		},
-		-- config = function()
-		-- 	local ibl = require("ibl")
-		-- 	local highlight = {
-		-- 		"RainbowRed",
-		-- 		"RainbowYellow",
-		-- 		"RainbowBlue",
-		-- 		"RainbowOrange",
-		-- 		"RainbowGreen",
-		-- 		"RainbowViolet",
-		-- 		"RainbowCyan",
-		-- 	}
-		--
-		-- 	-- create the highlight groups in the highlight setup hook, so they are reset
-		-- 	-- every time the colorscheme changes
-		-- 	local hooks = require("ibl.hooks")
-		-- 	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-		-- 		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-		-- 		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-		-- 		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-		-- 		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-		-- 		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-		-- 		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-		-- 		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-		-- 	end)
-		--
-		-- 	ibl.setup { indent = { highlight = highlight } }
-		-- end,
+				"RainbowCyan",
+			}
+
+			-- If the highlights are not supported by the used colorscheme,
+			-- create the highlight groups in the highlight setup hook, so they are reset
+			-- every time the colorscheme changes
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				local is_defined_hl = (next(vim.api.nvim_get_hl(0, {name=highlight[1]})) == nil)
+				if is_defined_hl then
+					vim.api.nvim_set_hl(0, "RainbowRed",    { fg = "#E06C75" })
+					vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+					vim.api.nvim_set_hl(0, "RainbowBlue",   { fg = "#61AFEF" })
+					vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+					vim.api.nvim_set_hl(0, "RainbowGreen",  { fg = "#98C379" })
+					vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+					vim.api.nvim_set_hl(0, "RainbowCyan",   { fg = "#56B6C2" })
+				end
+			end)
+
+			ibl.setup { indent = { highlight = highlight } }
+		end,
 	},
 
 }
