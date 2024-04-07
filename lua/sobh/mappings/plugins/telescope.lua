22b727c221e4e686f5fae20f8c2f5b6cfaf14eac
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-04-07 20:42:23 +0200

    NeoVim | Plugin - Telescope - Mappings : Added Git Status, and Commits.

--- lua/sobh/mappings/plugins/telescope.lua
+++ lua/sobh/mappings/plugins/telescope.lua
@@ -1,32 +1,36 @@
 ---- Telescope -----------------------------------------------------------------
 -- See `:help telescope.builtin`
-local telescope = require('telescope.builtin')
+local telescope = require("telescope.builtin")
 return {
 	n = {
-		['<leader>'] = {
-			[' '] = { telescope.buffers,     '[ ] Find existing Buffer' },
-			['/'] = {
+		["<leader>"] = {
+			[" "] = { telescope.buffers, "[ ] Find existing Buffer" },
+			["/"] = {
 				function()
 					-- You can pass additional configuration to telescope to change theme, layout, etc.
-					require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+					require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 						winblend = 10,
 						previewer = false,
-					})
+					}))
 				end,
-				'[/] Fuzzily search in current buffer'
+				"[/] Fuzzily search in current buffer",
 			},
 			f = {
-				name = 'Fuzzy/File Finder' ,
+				name = "Fuzzy/File Finder",
 				-- b  = { builtin.buffers,     '[F]ind [B]uffer' },
-				d  = { telescope.diagnostics, '[F]ind [D]ignostics' },
-				f  = { telescope.find_files,  '[F]ind [F]iles' },
-				g  = { telescope.git_files,   '[F]ind [G]it Files'},
-				h  = { telescope.help_tags,   '[F]ind [H]elp'},
-				r  = { telescope.oldfiles,    '[F]ind [R]ecently Opened Files' },
-				s  = { telescope.live_grep,   '[F]ind [S]tring in Files' },
-				w  = { telescope.grep_string, '[F]ind [W]ord under cursor in Files' },
-			}
-		}
-	}
+				d = { telescope.diagnostics, "[F]ind [D]ignostics" },
+				f = { telescope.find_files,  "[F]ind [F]iles" },
+				g = { telescope.git_files,   "[F]ind [G]it Files" },
+				h = { telescope.help_tags,   "[F]ind [H]elp" },
+				r = { telescope.oldfiles,    "[F]ind [R]ecently Opened Files" },
+				s = { telescope.live_grep,   "[F]ind [S]tring in Files" },
+				w = { telescope.grep_string, "[F]ind [W]ord under cursor in Files" },
+			},
+			g = {
+				name = "Git",
+				c = { telescope.git_commits, "Commits" },
+				a = { telescope.git_status,  "Status" },
+			},
+		},
+	},
 }
-
