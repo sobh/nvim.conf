35835587ea09db26dac8a15a982aab056d1a0df5
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-04-09 00:24:27 +0200

    NeoVim | lua ftplugin set keymap only for the current buffer.

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,1 +1,1 @@
-vim.keymap.set({'i', 'n'}, '<F2>', '<cmd>w<CR> <cmd>!lua %:p<CR>', { desc = 'Execute Current File' })
+vim.keymap.set({'i', 'n'}, '<F2>', '<cmd>w<CR> <cmd>!lua %:p<CR>', { desc = 'Execute Current File' , buffer=true })
